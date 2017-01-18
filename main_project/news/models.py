123cd307fd53ec4ddbc57b8dfcf3e415f778c93e
silviu <Silviu_Surciky@yahoo.com>
2017-01-18 15:37:30 +0200

    fixed model description field

--- main_project/news/models.py
+++ main_project/news/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 # Create your models here.
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
-    description = models.CharField(max)
+    description = models.CharField(max_length=200)
