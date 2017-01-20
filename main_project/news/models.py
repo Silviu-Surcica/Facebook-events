5897eaf01d0da52ac89c8262132fd4db03ee4e7e
silviu <Silviu_Surciky@yahoo.com>
2017-01-20 16:52:33 +0200

    updated Event model

--- main_project/news/models.py
+++ main_project/news/models.py
@@ -4,19 +4,17 @@ from django.utils import timezone
 
 from django.db import models
 
-# Create your models here.
-
 
 class Event(models.Model):
-    author = models.ForeignKey('auth.User')
+
     fb_id = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     type = models.CharField(max_length=200)
     cover_picture = models.URLField(max_length=255, blank=True, null=True, default=None)
     profile_picture = models.URLField(max_length=255, blank=True, null=True, default=None)
-    description = models.CharField(null=True, default=None, blank=True, max_length=1500)
-    distance = models.CharField(max_length=50)
+    description = models.TextField(null=True, default=None, blank=True)
+    distance = models.CharField(max_length=50, null=True, blank=True)
     start_time = models.DateTimeField(default=timezone.now)
-    end_time = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(default=timezone.now, null=True, blank=True)
     category = models.CharField(null=True, default=None, blank=True, max_length=50)
     stats = JSONField(default=None)
