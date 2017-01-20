8f20352af66f105fa48bd63490432ed1f4fae74d
silviu <Silviu_Surciky@yahoo.com>
2017-01-20 13:36:07 +0200

    added model for event

--- main_project/news/models.py
+++ main_project/news/models.py
@@ -1,11 +1,22 @@
 from __future__ import unicode_literals
+from django.contrib.postgres.fields import JSONField
+from django.utils import timezone
 
 from django.db import models
 
 # Create your models here.
 
 
-class Post(models.Model):
+class Event(models.Model):
     author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    fb_id = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    cover_picture = models.URLField(max_length=255, blank=True, null=True, default=None)
+    profile_picture = models.URLField(max_length=255, blank=True, null=True, default=None)
+    description = models.CharField(null=True, default=None, blank=True, max_length=1500)
+    distance = models.CharField(max_length=50)
+    start_time = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(default=timezone.now)
+    category = models.CharField(null=True, default=None, blank=True, max_length=50)
+    stats = models.TextField(default=None, blank=True, null=True)
